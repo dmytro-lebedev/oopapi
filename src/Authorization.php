881eec782d02cb2a0ede8cc70790cf269dbe2f29
dmytro-lebedev <dmytro.lebedev@gmail.com>
2014-02-16 06:08:32 +0200

    Cleanup

--- src/Authorization.php
+++ src/Authorization.php
@@ -2,21 +2,4 @@
 
 namespace oopapi;
 
-abstract class Authorization extends RequestHandler {
-    public function __invoke() {
-        if ($this->authorized()) {
-            $this->grant();
-        }
-        else {
-            $this->reject();
-        }
-    }
-    
-    abstract public function authorized();
-    
-    public function grant() {
-        parent::__invoke();
-    }
-
-    public function reject() {}
-}
+abstract class Authorization extends RequestHandler {}
