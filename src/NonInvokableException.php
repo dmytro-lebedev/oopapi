17f012a4dca2992020d84bf2523e1e2db09eb651
dmytro-lebedev <dmytro.lebedev@gmail.com>
2014-02-16 21:26:55 +0200

    Add protected property 'class' and its getter getClass()

--- src/NonInvokableException.php
+++ src/NonInvokableException.php
@@ -3,7 +3,14 @@
 namespace oopapi;
 
 class NonInvokableException extends \Exception {
+    protected $class;
+    
     public function __construct($class) {
+        $this->class = $class;
         parent::__construct("Class $class does not implement interface Invokable");
+    }
+    
+    public function getClass() {
+        return $this->class;
     }
 }
