e8f742b85b0b05b8090f29d152706038a7ec7e2a
dmytro-lebedev <dmytro.lebedev@gmail.com>
2014-02-16 05:52:38 +0200

    Add methods

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -10,8 +10,24 @@ abstract class RequestHandler implements Invokable {
     }
     
     public function __invoke() {
+        try {
+            $retval = $this->invoke();
+        } catch (Exception $e) {
+            $this->fail($e);
+            return;
+        }
+        $this->pass($retval);
+    }
+    
+    abstract protected function invoke();
+
+    protected function pass($retval) {
         if (isset($this->successor)) {
             $this->successor();
         }
+    }
+    
+    protected function fail(\Exception $e) {
+        throw $e;
     }
 }
