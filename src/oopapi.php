e5f211cf7c3b6a8eb51bbc34189f09ba73bee4a0
dmytro-lebedev <dmytro.lebedev@gmail.com>
2014-02-16 22:36:04 +0200

    Fix order

--- src/oopapi.php
+++ src/oopapi.php
@@ -5,6 +5,6 @@ require_once './AuthenticationException.php';
 require_once './AuthorizationException.php';
 require_once './Invokable.php';
 require_once './RequestHandler.php';
-require_once './Invokation.php';
 require_once './Authentication.php';
 require_once './Authorization.php';
+require_once './Invokation.php';
