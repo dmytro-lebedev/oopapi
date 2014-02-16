a4a788c87304dae585378392dd380d3368cb9214
dmytro-lebedev <dmytro.lebedev@gmail.com>
2014-02-16 06:01:22 +0200

    Change property visibility, use NonInvokableException

--- src/Invokation.php
+++ src/Invokation.php
@@ -4,15 +4,20 @@ namespace oopapi;
 
 class Invokation extends RequestHandler {
     const CLASS_VARIABLE = '__CLASS__';
-    private $class;
+    protected $class;
 
     public function __construct(RequestHandler $successor = NULL) {
         parent::__construct($successor);
         $this->class = $_POST[self::CLASS_VARIABLE];
     }
 
-    public function __invoke() {
+    protected function invoke() {
         $object = new $this->class;
-        if ($object instanceof Invokable) $object();
+        if ($object instanceof Invokable) {
+            return $object();
+        }
+        else {
+            throw new NonInvokableException($this->class);
+        }
     }
 }
